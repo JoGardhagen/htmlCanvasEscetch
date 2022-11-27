30cc33a63cd9fbfe49d65efcd4c6804e0197239d
Joakim <superapa.spanx@gmail.com>
2022-11-27 14:18:16 +0100

    contorller

--- scripts/controller.js
+++ scripts/controller.js
@@ -1,17 +1,45 @@
-function draw(){
+// function draw(){
     const canvas = document.querySelector('#drawingBoard');
-    if(!canvas.getContext){
-        return;
-    }
+    const {width, height } = canvas;
+    
+    // if(!canvas.getContext){
+    //     return;
+    // }
     const ctx = canvas.getContext('2d');
+
+    canvas.background = 'white';
 
     ctx.strokeStyle = 'red';
     ctx.lineWidth = 5;
 
-    ctx.beginPath();
-    ctx.moveTo(100,100);
-    ctx.lineTo(300,300);
-    ctx.stroke();
+    let moveAmount = 2;
 
-}
-draw();
+    let lastX = 0;
+    let lastY = 0;
+
+// }
+document.addEventListener("keydown",function(event){
+    console.log(event.witch);
+    if(event.keyCode === 37){
+        lastX -=moveAmount;
+    }
+    else if(event.keyCode === 38){
+        lastY -=moveAmount;
+    }
+    else if(event.keyCode === 39){
+        lastX +=moveAmount;
+    }
+    else if(event.keyCode === 40){
+        lastY +=moveAmount;
+    }
+    else if(event.keyCode === 21){
+        ctx.clear(0,0, canvas.width,canvas.height);
+    }
+
+    console.log(lastX,lastY)
+    event.preventDefault();
+
+    ctx.beginPath();
+    ctx.moveTo(lastX,lastY);
+    ctx.fillRect(lastX,lastY,moveAmount,moveAmount);
+});
